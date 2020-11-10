a900b9fe146e6529724b96b5d1bd7335d4557637
Murad <73176043+muraddagha@users.noreply.github.com>
2020-11-10 17:35:47 +0400

    Today task added

---
+++ 11102020/11102020/11102020/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11102020
+{
+    class Account
+    {
+         static int _totalNo;
+        private int _no;
+        public int No { get => _no; }
+        public string UserName;
+        private string Password;
+        public Account(string userName,string password)
+        {
+            _totalNo++;
+            _no = _totalNo;
+            this.UserName = userName;
+            this.Password = password;
+        }
+        public Account(string userName)
+        {
+            _totalNo++;
+            _no = _totalNo;
+            this.UserName = userName;
+            
+        }
+
+        public void IsUserNameValid()
+        {
+
+        }
+
+        public void IsPasswordValid()
+        {
+
+        }
+    }
+}
---
+++ 11102020/11102020/11102020/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _11102020
+{
+    static class ExMethods
+    {
+       
+        public static void Compare(this DateTime date,DateTime date1)
+        {
+           
+            if (date>=date1)
+            {
+                Console.WriteLine("True");
+            }
+           
+            else
+            {
+                Console.WriteLine("False");
+            }
+            
+        }
+    }
+    
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //DateTime date = new DateTime(2020,8,9);
+
+            //date.Compare(new DateTime(2020,10,31));
+            Account account = new Account("Murad");
+
+        }
+    }
+}
