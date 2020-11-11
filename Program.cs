270c5df4fd465768b3860529e63c5ec7a21db3d0
Murad <73176043+muraddagha@users.noreply.github.com>
2020-11-11 17:47:15 +0400

    (Task) 11.11.2020

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Murad Agha");
+            Student student2 = new Student("Sultan Sultanli");
+            Student student3 = new Student("Steve Jobs");
+            Student student4 = new Student("Steve Jobs");
+            Student student5 = new Student("Steve Jobs");
+            Student student6 = new Student("Steve Jobs");
+
+            List<Student> students = new List<Student>();
+            students.Add(student1);
+            students.Add(student2);
+            students.Add(student3);
+            students.Add(student4);
+            students.Add(student5);
+            students.Add(student6);
+            List<Student> findStu = students.FindAll(a => a.FullName == "Steve Jobs");
+
+            foreach (var item in findStu)
+            {
+                Console.WriteLine(item.No+"."+item.FullName);
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account
+{
+    class Student
+    {
+        private static int _totalNo;
+        private int _no;
+        public int No { get => _no; }
+        public string FullName;
+        public Student(string fullName)
+        {
+            _totalNo++;
+            _no = _totalNo;
+            this.FullName = fullName;
+        }
+
+    }
+}
