8e80999058ff2e2ec310ea86d4a66aebca45fed6
Murad <73176043+muraddagha@users.noreply.github.com>
2020-11-12 17:52:50 +0400

    11122020(Task)

---
+++ 11.12.2020/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Contains
+{
+    class Program
+    {
+        //public static bool Contains(List<int> list1,List<int> list2)
+        //{
+        //    foreach (var item1 in list1)
+        //    {
+        //        if (!list2.Contains(item1))
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return true; ;
+        //}
+        //public static bool IsListEqual(List<int> list1, List<int> list2)
+        //{
+        //        if (list1.SequenceEqual(list2))
+        //        {
+        //            return true;
+        //        } 
+                
+        //    return false;
+            
+        //}
+
+        static void Main(string[] args)
+        {
+            Student Student1 = new Student(1, "Murad");
+            Console.WriteLine(Student1.Name);
+
+            //List<int> MyList1 = new List<int>();
+            //List<int> MyList2 = new List<int>();
+            //MyList1.Add(15);
+            //MyList1.Add(17);
+            //MyList1.Add(1);
+            //MyList2.Add(15);
+            //MyList2.Add(17);
+            //MyList2.Add(1);
+
+            //Console.WriteLine(Program.IsListEqual(MyList1, MyList2));
+        }
+    }
+}
---
+++ 11.12.2020/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contains
+{
+    class Student
+    {
+        public int GroupNo;
+        public readonly string Name;
+       
+        public bool IsNameValid(string name)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (!char.IsLetter(name[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public Student(int groupNo, string name)
+        {
+            this.GroupNo = groupNo;
+            if (IsNameValid(name))
+            {
+                this.Name = name;
+            }
+            
+        }
+    }
+}
